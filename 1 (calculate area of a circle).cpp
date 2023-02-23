d2b9f65083527e78b074757ad4d8fdf3a01b4f34
AS2345DF <125998247+AS2345DF@users.noreply.github.com>
2023-02-23 07:54:33 -0800

    Add files via upload

---
+++ 1 (calculate area of a circle).cpp
@@ -0,0 +1,13 @@
+// calculate the area of circle
+#include <iostream>
+using namespace std;
+int main()
+{
+int radius, area;
+cout<<"Enter the radius of circle: ";
+cin>>radius;
+area=3.14*radius*radius;
+cout<<"Area of circle is: ";
+cout<<area;
+}
+
